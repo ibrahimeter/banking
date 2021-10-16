c128db7887bc88625058c63c167825fde21478b5
ibrahim eter <ibrahimeter75@gmail.com>
2021-10-16 18:05:38 +0200

    add transfer and deposit balance api2

--- banking/urls.py
+++ banking/urls.py
@@ -2,8 +2,13 @@ from django.urls import path
 
 from banking.views import create_account
 from banking.views import my_accounts
+import banking.views as views
+
 
 urlpatterns = [
     path('create_account/',create_account),
     path('my_accounts/', my_accounts),
+    path('deposit/',views.deposit),
+    path('transfer/', views.transfer),
+
 ]
